c978f60742397663ed4e85d744da2e8b496071f2
Eli DeWitt <65370615+elidewitt@users.noreply.github.com>
2020-09-17 14:19:20 -0500

    Add files via upload

---
+++ source.js
@@ -0,0 +1,70 @@
+const pitScouter = {
+  "Pregame" : {
+    "General Information" : {
+      "Scouter_Name" : {
+        "Element" : "input",
+        "type" : "text"
+      },
+      "Match_Number" : {
+        "Element" : "input",
+        "type" : "number"
+      },
+      "Team_Number" : {
+        "Element" : "select",
+        "type" : "number"
+      },
+      "No_Show?" : {
+        "Element" : "input",
+        "type" : "checkbox"
+      }
+    },
+    "Alliance" : {
+      "Red" : {
+        "Element" : "input",
+        "type" : "radio",
+        "value" : "red",
+        "name" : "alliance"
+      },
+      "Blue" : {
+        "Red" : {
+          "Element" : "input",
+          "type" : "radio",
+          "value" : "blue",
+          "name" : "alliance"
+        }
+      }
+    },
+    "StartPosition" : {
+      "Far" : {
+        "Element" : "input",
+        "type" : "radio",
+        "value" : "far",
+        "name" : "startPosition"
+      },
+      "Middle" : {
+        "Element" : "input",
+        "type" : "radio",
+        "value" : "middle",
+        "name" : "startPosition"
+      },
+      "Close" : {
+        "Element" : "input",
+        "type" : "radio",
+        "value" : "close",
+        "name" : "startPosition"
+      }
+    }
+  },
+  "Autonomous" : {
+
+  },
+  "Tele-Op" : {
+
+  },
+  "Endgame" : {
+
+  },
+  "Postgame" : {
+
+  }
+}
